1c890f6ac0c4357529800f5e89f65c31476bfe61
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-07-13 22:22:13 +0500

    working on verify jwt

--- middlewares/verifyJWT.js
+++ middlewares/verifyJWT.js
@@ -9,12 +9,20 @@ const verifyJWT = (req, res, next) => {
   }
 
   //jwt.verify function returns the decoded token
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ success: false, message: "Invalid token" });
-    }
-    req.email = decoded.userInfo.email;
-    // req.
-  });
+  try {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ success: false, message: "Invalid token" });
+      }
+      // set user object in req
+      req.email = decoded.userInfo.email;
+      next();
+    });
+  } catch (err) {
+    console.log("Error:", err.message);
+    res.status(403).json({ message: "Invalid token", success: false });
+  }
 };
